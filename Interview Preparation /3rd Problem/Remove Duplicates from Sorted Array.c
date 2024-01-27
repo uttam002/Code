8ad08ba4428b5316a3b589a4c8beab758d74d6dc
Uttam Bhuva <112840672+uttam002@users.noreply.github.com>
2024-01-28 00:42:10 +0530

    26. Remove Duplicates from Sorted Array

---
+++ Interview Preparation /3rd Problem/Remove Duplicates from Sorted Array.c
@@ -0,0 +1,15 @@
+int removeDuplicates(int* nums, int numsSize) {
+     if (numsSize == 0) {
+            return 0;
+        }
+
+        int k = 1; // Update pointer
+
+        for (int i = 1; i < numsSize; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k++] = nums[i];
+            }
+        }
+
+        return k;
+}
