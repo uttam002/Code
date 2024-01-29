27230a7bc774b93b7b84d3efb289fdacfe1f570d
Uttam Bhuva <112840672+uttam002@users.noreply.github.com>
2024-01-29 08:30:32 +0530

    169. Majority Element

---
+++ Interview Preparation /7th Problem/Majority Elements.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+       int candidate = nums[0];
+        int count = 1;
+
+        // Boyer-Moore Voting Algorithm
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                candidate = nums[i];
+                count = 1;
+            } else if (nums[i] == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+
+    }
+}
