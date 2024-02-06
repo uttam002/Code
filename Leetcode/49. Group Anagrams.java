9fc6d4bfdd72d076535e9ff73e92601f7c9d76cb
Uttam Bhuva <112840672+uttam002@users.noreply.github.com>
2024-02-06 14:08:19 +0530

    Update 49. Group Anagrams.java

--- Leetcode/49. Group Anagrams.java
+++ Leetcode/49. Group Anagrams.java
@@ -1,40 +1,4 @@
 class Solution {
-    // public static boolean areAnagrams(String str1, String str2) {
-    //     // Check if lengths are different, if so, they can't be anagrams
-    //     if (str1.length() != str2.length()) {
-    //         return false;
-    //     }
-
-    //     // Initialize an array to store the frequency of each character
-    //     int[] charFrequency = new int[256]; // Assuming ASCII characters
-
-    //     // Count the frequency of characters in the first string
-    //     for (char ch : str1.toCharArray()) {
-    //         charFrequency[ch]++;
-    //     }
-
-    //     // Subtract the frequency of characters in the second string
-    //     for (char ch : str2.toCharArray()) {
-    //         charFrequency[ch]--;
-    //     }
-
-    //     // If all entries in the charFrequency array are 0, the strings are anagrams
-    //     for (int frequency : charFrequency) {
-    //         if (frequency != 0) {
-    //             return false;
-    //         }
-    //     }
-
-    //     return true;
-    // }
-    // Helper method to create a list of String
-    // private static List<String> createIntegerList(String elements) {
-    //     List<String> innerList = new ArrayList<>();
-    //     for (String element : elements) {
-    //         innerList.add(element);
-    //     }
-    //     return innerList;
-    // }
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> hashMap = new HashMap<>();
 
@@ -54,15 +18,7 @@ class Solution {
                 hashMap.put(sortedStr, newList);
             }
         }
-        
-        
-        return new ArrayList<>(hashMap.values());
-
-        // int count=0;
-       
-        //     for (String element : strs) {
-        //     innerList.add(element);
-        // }
-        
+         return new ArrayList<>(hashMap.values());
+        }
     }
 }
