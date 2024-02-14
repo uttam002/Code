e07136abdc14f897b518d6dd29934e6b9906a671
Uttam Bhuva <112840672+uttam002@users.noreply.github.com>
2024-02-14 14:18:10 +0530

    13 Feb 2024

---
+++ Leetcode/2108. Find First Palindromic String in the Array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static String firstPalindrome(String[] words) {
+        for (String word : words) {
+            if (isPalindrome(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+
+    public static boolean isPalindrome(String word) {
+        int left = 0;
+        int right = word.length() - 1;
+        
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
