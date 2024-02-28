1c004eb3296f114e373aef7f5eb0885529b044d3
Uttam Bhuva <112840672+uttam002@users.noreply.github.com>
2024-02-28 23:49:33 +0530

    28 Feb 2024

---
+++ Leetcode/513. Find Bottom Left Tree Value.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        TreeNode leftmost = null;
+        
+        while (!queue.isEmpty()) {
+            leftmost = queue.peek();
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+        }
+        
+        return leftmost.val;
+    
+    }
+}
