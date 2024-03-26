206158209ac75198066a57df7e6a596496d8424e
Uttam Bhuva <112840672+uttam002@users.noreply.github.com>
2024-03-26 12:31:11 +0530

    26 March 2024

---
+++ Leetcode/41. First Missing Positive.c
@@ -0,0 +1,7 @@
+int firstMissingPositive(int* nums, int numsSize) {
+    int n = numsSize;
+    for (int i = 0; i < n; i++)if (nums[i] <= 0)nums[i] = n + 1;
+    for (int i = 0; i < n; i++)if (abs(nums[i]) <= n && nums[abs(nums[i]) - 1] > 0)nums[abs(nums[i]) - 1] *= -1;
+    for (int i = 0; i < n; i++)if (nums[i] > 0)return i + 1;
+    return n + 1;
+}
