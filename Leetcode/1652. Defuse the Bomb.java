63d4ac885363eea1eda5f492640528e143f1d86e
Uttam Bhuva <112840672+uttam002@users.noreply.github.com>
2024-11-18 15:50:50 +0530

    Day 6

--- Leetcode/1652. Defuse the Bomb.java
+++ Leetcode/1652. Defuse the Bomb.java
@@ -3,12 +3,24 @@ class Solution {
         int n = code.length;
         int[] result = new int[n];
 
-        if (k == 0) return result;
+        if (k == 0) {
+            // If k is 0, all elements are replaced with 0
+            return result;
+        }
 
         for (int i = 0; i < n; i++) {
             int sum = 0;
-            if (k > 0) for (int j = 1; j <= k; j++) sum += code[(i + j) % n];
-            else for (int j = 1; j <= -k; j++) sum += code[(i - j + n) % n];
+            if (k > 0) {
+                // Sum the next k elements (circularly)
+                for (int j = 1; j <= k; j++) {
+                    sum += code[(i + j) % n];
+                }
+            } else {
+                // Sum the previous |k| elements (circularly)
+                for (int j = 1; j <= -k; j++) {
+                    sum += code[(i - j + n) % n];
+                }
+            }
             result[i] = sum;
         }
 
