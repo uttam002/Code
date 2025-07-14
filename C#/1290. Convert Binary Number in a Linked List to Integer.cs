594d324b79504824a5b1c2feeaf6fcaf86a79953
Uttam Bhuva <112840672+uttam002@users.noreply.github.com>
2025-07-14 09:52:42 +0530

    Create 1290. Convert Binary Number in a Linked List to Integer

---
+++ C#/1290. Convert Binary Number in a Linked List to Integer.cs
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public int GetDecimalValue(ListNode head) {
+        int num = 0;
+        while (head != null) {
+            num = (num << 1) | head.val;
+            head = head.next;
+        }
+        return num;
+    }
+}
